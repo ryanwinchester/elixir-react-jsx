f59cf3c45dc6e3db6d7d712a40a86ee62c19e0d8
fungku <fungku@gmail.com>
2015-03-21 02:53:52 -0700

    actually add the files

---
+++ index.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var react = require('gulp-react');
+var concat = require('gulp-concat');
+
+elixir.extend("jsx", function(src, outputFile, dest) {
+    src = src || 'resources/assets/jsx/**/*.jsx';
+    dest = dest || 'public/js';
+    outputFile = outputFile || 'app-react.js';
+
+    gulp.task("jsx", function() {
+        gulp.src(src)
+            .pipe(react())
+            .pipe(concat(outputFile))
+            .pipe(gulp.dest(dest));
+    });
+
+    return this.queueTask("jsx");
+});
